c63af44900d0b5ad5fe8f5927b0eeb91406531af
1CarlosRenan <carlosrenan96@gmail.com>
2022-08-17 21:20:46 -0300

    Testando a Api

---
+++ src/api/Api.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import UserPost from './endpoints/UserPost';
+
+const Api = () => {
+  return (
+    <div>
+      <h2>USER POST</h2>
+      <UserPost />
+    </div>
+  );
+};
+
+export default Api;
