8ade8315bee6307fc087ce63aa650fe4f7536b08
1CarlosRenan <carlosrenan96@gmail.com>
2022-08-20 22:47:20 -0300

    Estrutura inicial e setando configurações gerais de estilização

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
 import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
+import Header from './Components/Header';
+import Home from './Components/Home';
+import Login from './Components/Login/Login';
+import Footer from './Components/Footer';
+
 import './App.css';
-import Api from './api/Api';
 
 function App() {
   return (
     <div>
-      <Api />
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
     </div>
   );
 }
