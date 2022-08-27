1a69ebdb0843de6d9bc3716916da193aba923489
1CarlosRenan <carlosrenan96@gmail.com>
2022-08-26 21:21:28 -0300

    Incrementando o ProtectedRoute

--- src/App.js
+++ src/App.js
@@ -4,10 +4,12 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Header from './Components/Header';
 import Home from './Components/Home';
 import Login from './Components/Login/Login';
+import User from './Components/User/User';
 import Footer from './Components/Footer';
 import { UserStorage } from './UserContext';
 
 import './App.css';
+import ProtectedRoute from './Components/Helper/ProtectedRoute';
 
 function App() {
   return (
@@ -17,7 +19,15 @@ function App() {
           <Header />
           <Routes>
             <Route path="/" element={<Home />} />
-            <Route path="/login/*" element={<Login />} />
+            <Route path="login/*" element={<Login />} />
+            <Route
+              path="conta/*"
+              element={
+                <ProtectedRoute>
+                  <User />
+                </ProtectedRoute>
+              }
+            />
           </Routes>
           <Footer />
         </UserStorage>
