d0ee4ea9e06845d962c927973cd899649acad752
1CarlosRenan <carlosrenan96@gmail.com>
2022-09-05 19:21:29 -0300

    Incrementando rota do perfil

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import { UserStorage } from './UserContext';
 import User from './Components/User/User';
 import ProtectedRoute from './Components/Helper/ProtectedRoute';
 import Photo from './Components/Photo/Photo';
+import UserProfile from './Components/User/UserProfile';
 
 function App() {
   return (
@@ -28,6 +29,7 @@ function App() {
               }
             />
             <Route path="foto/:id" element={<Photo />} />
+            <Route path="perfil/:user" element={<UserProfile />} />
           </Routes>
           <Footer />
         </UserStorage>
