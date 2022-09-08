b09bfaa3f24736ad2edc6666d4ba54c6d8a188e3
1CarlosRenan <carlosrenan96@gmail.com>
2022-09-08 20:30:43 -0300

    Importante Victory e incrementando componentes de gráficos

--- src/Components/User/UserStats.js
+++ src/Components/User/UserStats.js
@@ -1,9 +1,32 @@
 import React from 'react';
 import Head from '../Helper/Head';
+import useFetch from '../../Hooks/useFetch';
+import { STATS_GETS } from '../../api';
+import Loading from '../Helper/Loading';
+import Error from '../Helper/Error';
+import UserStatsGraphs from './UserStatsGraphs';
 
 const UserStats = () => {
-  <Head title="Estatísticas" />;
-  return <div>Estatísticas</div>;
+  const { data, error, loading, request } = useFetch();
+
+  React.useFetch(() => {
+    async function getData() {
+      const { url, options } = STATS_GETS();
+      await request(url, options);
+    }
+    getData();
+  }, [request]);
+
+  if (loading) return <Loading />;
+  if (error) return <Error />;
+  if (data) {
+    return (
+      <div>
+        <Head title="Estatísticas" />; Estatística
+        <UserStatsGraphs data={data} />
+      </div>
+    );
+  } else return null;
 };
 
 export default UserStats;
